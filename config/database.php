d4e2402542ba02af6559d363be0a7a056b94c5d1
alinmihai04 <72596650+alinmihai04@users.noreply.github.com>
2021-08-17 15:52:39 +0300

    test1

--- config/database.php
+++ config/database.php
@@ -31,11 +31,12 @@ return [
     |
     */
 
+
     'connections' => [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env(env('APP_PROD') ? 'PROD_DB_DATABASE' : 'DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
 
@@ -43,9 +44,9 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env(env('APP_PROD') ? 'PROD_DB_DATABASE' : 'DB_DATABASE', 'forge'),
+            'username' => env(env('APP_PROD') ? 'PROD_DB_USERNAME' : 'DB_USERNAME', 'forge'),
+            'password' => env(env('APP_PROD') ? 'PROD_DB_PASSWORD' : 'DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
